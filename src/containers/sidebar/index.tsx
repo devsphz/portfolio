aa7993dbe42a84a7e841d14ffa8279db688f93e4
Pedro Henrique <phenriquesz.1000@gmail.com>
2025-02-25 21:05:57 -0300

    Estilizando a sidebar

--- src/containers/sidebar/index.tsx
+++ src/containers/sidebar/index.tsx
@@ -1,9 +1,18 @@
+import Avatar from "../../components/avatar";
+import Paragrafo from "../../components/paragrafo";
 import Titulo from "../../components/titulo";
+
+import { BotaoTema, Descricao, SidebarContainer  } from './styles';
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/devsphz.png" alt="Foto de Pedro Henrique" />
+    <SidebarContainer>
+    <Avatar/>
     <Titulo fontSize={20}>Pedro Henrique</Titulo>
+    <Paragrafo tipo="secundario" fontSize={16} >Devsphz</Paragrafo>
+    <Descricao tipo="principal" fontSize={12}>Engenheiro Front-End</Descricao>
+    <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 );
 
